6695aabe4c9681a534cf1c43ac3fba96cd4a1b74
Gun Gun Priatna <gungunpriatna02@gmail.com>
2026-04-11 13:35:46 +0700

    modify route

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,16 @@ Route::get('/', function () {
     return view('home');
 });
 
-
 Route::get('/entries', [EntryController::class, 'index']);
-Route::get('/entries/{entry}', [EntryController::class, 'show']);
 
+Route::middleware('auth')->group(function () {
+    Route::get('/entries/create', [EntryController::class, 'create']);
+    Route::post('/entries', [EntryController::class, 'store']);
+    Route::get('/entries/{entry}', [EntryController::class, 'show']);
+});
+
+// ONLY FOR DEVELOPMENT - delete after lesson 10
+Route::get('/dev-login', function () {
+    auth()->loginUsingId(1);
+    return redirect('/entries');
+});
