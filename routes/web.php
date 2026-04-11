0f2869e5dab9709cf7ced8cd86cce040cf3be418
Gun Gun Priatna <gungunpriatna02@gmail.com>
2026-04-11 09:15:21 +0700

    create home page and entries page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,30 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/entries', function () {
+    $entries = [
+        [
+            'id'         => 1,
+            'title'      => 'Year-end vacation plans',
+            'content'    => 'It has been a while since the last vacation. Maybe Yogyakarta or Lombok. Need to research the budget and best timing.',
+            'created_at' => '20 February 2026',
+        ],
+        [
+            'id'         => 2,
+            'title'      => 'First day learning Laravel',
+            'content'    => 'Started learning Laravel today. Turns out it is not as hard as I expected. Routing and views are quite intuitive.',
+            'created_at' => '19 February 2026',
+        ],
+        [
+            'id'         => 3,
+            'title'      => 'This month\'s resolutions',
+            'content'    => 'Want to be more consistent writing entries every day. At least one paragraph before bed.',
+            'created_at' => '18 February 2026',
+        ],
+    ];
+
+    return view('entries.index', compact('entries'));
 });
