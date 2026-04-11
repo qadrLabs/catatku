049a080ebdd018e471d13eb337f707da5b7a49ed
Gun Gun Priatna <gungunpriatna02@gmail.com>
2026-04-11 11:39:55 +0700

    add fillable attribute and belongsTo relation

--- app/Models/Entry.php
+++ app/Models/Entry.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[Fillable(['title', 'content'])]
 class Entry extends Model
 {
-    //
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
